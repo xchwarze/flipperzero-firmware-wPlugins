2f4f54825a6c2e2fd30ad8ef3c6defe791c90b8d
jblanked <82678820+jblanked@users.noreply.github.com>
2024-11-04 17:52:56 -0500

    Remove connect WiFi from init
    FlipperHTTP auto-connects

--- app.c
+++ app.c
@@ -12,17 +12,15 @@ int32_t flip_weather_app(void *p)
 
     // Initialize the FlipWeather application
     FlipWeatherApp *app = flip_weather_app_alloc();
+    if (!app)
+    {
+        FURI_LOG_E(TAG, "Failed to allocate FlipWeatherApp");
+        return -1;
+    }
 
     if (!flipper_http_ping())
     {
         FURI_LOG_E(TAG, "Failed to ping the device");
-        return -1;
-    }
-
-    // connect wifi
-    if (!flipper_http_connect_wifi())
-    {
-        FURI_LOG_E(TAG, "Failed to connect to WiFi");
         return -1;
     }
 
--- flip_weather_free.h
+++ flip_weather_free.h
@@ -10,11 +10,6 @@ static void flip_weather_app_free(FlipWeatherApp *app)
         return;
     }
 
-    if (!flipper_http_disconnect_wifi())
-    {
-        FURI_LOG_E(TAG, "Failed to disconnect from wifi");
-    }
-
     // Free View(s)
     if (app->view_weather)
     {
